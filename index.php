9edf219d2c89efc448fb1fff3abaccc29802c73a
rekky1aws <killikomfr@gmail.com>
2023-05-07 19:07:20 +0200

    Initialization

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Daylii </title>
+</head>
+<body>
+	<header>
+		<h1> Daylii </h1>
+	</header>
+</body>
+</html>

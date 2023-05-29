6311fdf7d64fed8deba45a8572911babe6d97ea9
rekky1aws <killikomfr@gmail.com>
2023-05-29 19:46:44 +0200

    Added security to the task updater

--- controller/updateTask.php
+++ controller/updateTask.php
@@ -1,9 +1,19 @@
 <?php 
 require_once "../model/TaskDB.php";
 
-$taskDB = new TaskDB("mysql", "127.0.0.1", "3306", "daylii", "root", "");
+try {
+	// Access to DB
+	$taskDB = new TaskDB("mysql", "127.0.0.1", "3306", "daylii", "root", "");
 
-$taskDB->updateTask($_GET['id']);
-header("Location: /");
+	// Update Request
+	$taskDB->updateTask($_GET['id']);
+
+	// Going back to main page
+	header("Location: /");
+} catch (Exception $e) {
+	die($e->getMessage());
+}
+
+
 
 ?>

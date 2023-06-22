0039981933006dd6c76f44e7d428640ef9fc5462
rekky1aws <killikomfr@gmail.com>
2023-06-22 20:26:03 +0200

    Moved more common parts of HTML head to the component
    Moved viewport and charset meta to the Head components to avoid
    repetition.

--- components/head.php
+++ components/head.php
@@ -3,6 +3,8 @@
 class Head
 {
 	private $content = "
+	<meta charset=\"utf-8\">
+	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 	<link rel=\"stylesheet\" type=\"text/css\" href=\"/style/main.css\">
 	";
 
